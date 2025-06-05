2bde87cd518e57b20653a9a25962e0553534829c
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-06-05 13:35:59 -0400

    remove timing logic from shell profile
    accidentally left in

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,3 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
 
-# Time the CoreUtils module import
-$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 Import-Module CoreUtils -Global -DisableNameChecking
-$stopwatch.Stop()
-
-# Log the result
-$loadTime = $stopwatch.ElapsedMilliseconds
-Write-Host "CoreUtils module loaded in $loadTime ms" -ForegroundColor Green
