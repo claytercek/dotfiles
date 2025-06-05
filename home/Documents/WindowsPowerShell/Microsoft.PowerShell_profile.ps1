f1ea8c81d60dc0463d2f3e363f044bc8909dd318
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-06-05 13:47:06 -0400

    add zoxide

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
-
 Import-Module CoreUtils -Global -DisableNameChecking
+
+Invoke-Expression (& { (zoxide.exe init powershell | Out-String) })
