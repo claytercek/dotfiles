2d8d7b1dfa1eece8f2c9d35ab03a686bfa0ab755
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-07-14 09:51:13 -0400

    add .claude to gitignore, use starship for pwsh

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,12 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
+# Enable oh-my-posh
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
+
+# Enable starship
+Invoke-Expression (&starship init powershell)
+# Enable-TransientPrompt
+
+# enable Uutils CoreUtils
 Import-Module CoreUtils -Global -DisableNameChecking
 
+# Enable zoxide
 Invoke-Expression (& { (zoxide.exe init powershell | Out-String) })
