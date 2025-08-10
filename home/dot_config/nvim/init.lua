41aa71fa074ec2a9406cedaf0897d377d8e7fdd7
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-08-10 11:22:18 -0400

    add nvim and ghostty configs

---
+++ home/dot_config/nvim/init.lua
@@ -0,0 +1,56 @@
+vim.cmd([[set mouse=]])
+vim.opt.winborder = "rounded"
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+
+local map = vim.keymap.set
+vim.g.mapleader = " "
+map('n', '<leader>o', ':update<CR> :source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+map('n', '<leader>v', ':e $MYVIMRC<CR>')
+map('n', '<leader>z', ':e ~/.config/zsh/.zshrc<CR>')
+map('n', '<leader>s', ':e #<CR>')
+map('n', '<leader>S', ':sf #<CR>')
+map({ 'n', 'v' }, '<leader>y', '"+y')
+map({ 'n', 'v' }, '<leader>d', '"+d')
+map({ 'n', 'v' }, '<leader>c', '1z=')
+
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+})
+
+require "mason".setup()
+require "mini.pick".setup()
+require "oil".setup()
+
+map('n', '<leader>f', ":Pick files<CR>")
+map('n', '<leader>h', ":Pick help<CR>")
+map('n', '<leader>e', ":Oil<CR>")
+map('t', '', "")
+map('t', '', "")
+map('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.lsp.enable({ "lua_ls", "svelte" })
+require'nvim-treesitter'.setup({ highlight = { enable = true, }, })
+
+-- colors
+require "vague".setup({ transparent = true })
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+
