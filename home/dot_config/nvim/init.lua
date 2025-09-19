7fd29fe0de99d03a32132bc378e94b3802a4d5cf
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-09-19 14:19:21 -0400

    track zed config, add wake script

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -32,7 +32,6 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
 	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
 require "mason".setup()
